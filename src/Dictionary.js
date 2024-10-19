68d5ab6d2219efdc65fffb053513bceed39b1b3d
Robyn <127926809+Robyn-web@users.noreply.github.com>
2024-10-19 13:59:05 +0100

    Add: Added {useState} to Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Dictionary.css';
 
 export default function Dictionary() {
+  let [keyword, setkeyword] = useState('');
+  function search(event) {
+    event.preventDefault();
+    alert('Say something');
+  }
   return (
     <div className="Dictionary">
-      <form>
+      <form onSubmit={search}>
         {/* COMMENT - The autoFocus attribute ensures that when the component is rendered, the input field is already selected and user can start typing, instead of the user manually clicking on the box to start typing */}
         <input type="search" autoFocus={true} />
       </form>
