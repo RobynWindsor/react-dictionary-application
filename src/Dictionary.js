23dc4877349f135ed60adf82a835bf7d41e5f980
Robyn <127926809+Robyn-web@users.noreply.github.com>
2024-10-19 13:47:27 +0100

    Add: Added form tag to Dictionary.js - autofocus attribute added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,5 +2,12 @@ import React from 'react';
 import './Dictionary.css';
 
 export default function Dictionary() {
-  return <div className="Dictionary">Hell from Dictionary</div>;
+  return (
+    <div className="Dictionary">
+      <form>
+        {/* COMMENT - The autoFocus attribute ensures that when the component is rendered, the input field is already selected and user can start typing, instead of the user manually clicking on the box to start typing */}
+        <input type="search" autoFocus={true} />
+      </form>
+    </div>
+  );
 }
