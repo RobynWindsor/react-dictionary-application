98138b8c8744093e92e3f8c681625a9fac98492f
Robyn <127926809+Robyn-web@users.noreply.github.com>
2024-10-20 09:10:52 +0100

    Add: Found keyword value in target.value

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Hello</header>
+        <header className="App-header">World Dictionary</header>
         <main>
           <Dictionary />
         </main>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,13 +3,18 @@ import './Dictionary.css';
 
 export default function Dictionary() {
   let [keyword, setkeyword] = useState('');
+
   function search(event) {
     event.preventDefault();
-    alert('Say something');
+    alert(`Searching for ${keyword}`);
+  }
+
+  function handleKeywodChange(event) {
+    setkeyword(event.target.value);
   }
   return (
     <div className="Dictionary">
-      <form onSubmit={search}>
+      <form onSubmit={search} onChange={handleKeywodChange}>
         {/* COMMENT - The autoFocus attribute ensures that when the component is rendered, the input field is already selected and user can start typing, instead of the user manually clicking on the box to start typing */}
         <input type="search" autoFocus={true} />
       </form>
