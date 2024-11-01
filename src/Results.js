224c97ec25a32fef5c9aa1101dd419ddf8a5a149
Robyn <127926809+Robyn-web@users.noreply.github.com>
2024-11-01 15:17:52 +0000

    Update: if statement in Results component to return 'null' if else

--- src/Results.js
+++ src/Results.js
@@ -3,5 +3,7 @@ import React from 'react';
 export default function Results(props) {
   if (props.results) {
     return <div className="Results">Hello from Results</div>;
+  } else {
+    return null;
   }
 }
