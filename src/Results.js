6dadd25ac963c607b2e30c781872427cfcaca6b7
Robyn <127926809+Robyn-web@users.noreply.github.com>
2024-11-01 15:16:49 +0000

    Update: if statement in Results Component

--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
-export default function Results() {
-  return <div className="Results">Hello from Results</div>;
+export default function Results(props) {
+  if (props.results) {
+    return <div className="Results">Hello from Results</div>;
+  }
 }
