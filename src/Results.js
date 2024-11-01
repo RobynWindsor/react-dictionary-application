704e13d1ebe3558ea5b82acff3b7b059f45087ce
Robyn <127926809+Robyn-web@users.noreply.github.com>
2024-11-01 16:11:46 +0000

    Update: Updating if statement in Results.js

--- src/Results.js
+++ src/Results.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 export default function Results(props) {
-  if (props.results) {
+  //These props are getting results from property called results of Results
+
+  // This props.results.length - we do this to check the length of the array before we render the contetn below in the return statement. If length is 0 it indicates that there are no results available and returning null prevents rendering an empty or irrelevant UI.
+  if (props.results.length) {
     return <div className="Results">Hello from Results</div>;
   } else {
     return null;
