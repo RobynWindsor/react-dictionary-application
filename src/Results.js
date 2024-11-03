67d1828f20940e722fb2e293fe5a0e04127cd668
Robyn <127926809+Robyn-web@users.noreply.github.com>
2024-11-03 11:25:00 +0000

    Fix: props.result to props.results

--- src/Results.js
+++ src/Results.js
@@ -4,7 +4,7 @@ export default function Results(props) {
   //These props are getting results from property called results of Results
 
   // This props.results.length - we do this to check the length of the array before we render the contetn below in the return statement. If length is 0 it indicates that there are no results available and returning null prevents rendering an empty or irrelevant UI.
-  if (props.results.length) {
+  if (props.results) {
     return <div className="Results">Hello from Results</div>;
   } else {
     return null;
