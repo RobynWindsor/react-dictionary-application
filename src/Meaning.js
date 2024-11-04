d956d3d586e01e4c9acc17aeec7819a606a52339
Robyn <127926809+Robyn-web@users.noreply.github.com>
2024-11-04 22:02:37 +0000

    Add basic structure for Meaning component to display part of speech, definition, and example

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,6 +5,9 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.map()}
+      <p>{props.meaning.definitions[0].definition}</p>
+      <p>{props.meaning.definitions[0].example}</p>
     </div>
   );
 }
