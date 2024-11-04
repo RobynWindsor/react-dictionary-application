fd9156b32fc166623e1793931cd23770dee1465f
Robyn <127926809+Robyn-web@users.noreply.github.com>
2024-11-04 21:53:42 +0000

    Add: Meaning component to display part of speech

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,5 +2,9 @@ import React from 'react';
 
 export default function Meaning(props) {
   console.log(props.meaning);
-  return 'Hello from meaning';
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+    </div>
+  );
 }
